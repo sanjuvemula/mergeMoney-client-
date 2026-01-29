63c6fcca1a789c78cf44b404d7356192619e5bef
Aditya Solge <aditya.solge001@umb.edu>
2026-01-29 11:21:00 +0530

    Login form

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,45 @@ import { Link } from "react-router-dom";
 
 function Header() {
     return (
-        <div className="container">
-            <Link to='/'>Home</Link>
-            <Link to='/login'>Login</Link>
-        </div>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">
+                    ExpenseApp
+                </Link>
+                <button
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                    className="navbar-toggler"
+                    data-bs-target="#navbarSupportedContent"
+                    data-bs-toggle="collapse"
+                    type="button"
+                >
+                    <span className="navbar-toggler-icon" />
+                </button>
+                <div
+                    className="collapse navbar-collapse"
+                    id="navbarSupportedContent"
+                >
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link
+                                aria-current="page"
+                                className="nav-link active"
+                                to="/"
+                            >
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/login">
+                                Login
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     );
 }
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,83 @@
+import { useState } from "react";
+
+
 function Login() {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+    const [errors, setErrors] = useState({});
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+
+    const validate = () => {
+        let newErrors = {};
+        let isValid = true;
+
+        if (formData.email.length === 0) {
+            newErrors.email = "Email is required";
+            isValid = false;
+        }
+
+        if (formData.password.length === 0) {
+            newErrors.password = "Password is required";
+            isValid = false;
+        }
+
+        setErrors(newErrors);
+        return isValid;
+    };
+
+    const handleFormSubmit = (event) => {
+        // Prevent default behaviour of form which is to do complete page reload.
+        event.preventDefault(); 
+
+        if (validate()) {
+            console.log('Valid Form');
+        } else {
+            console.log('Invalid Form');
+        }
+    };
+
     return (
-        <div className="container">
-            <h3 className="text-center">Signin to continue...</h3>
+        <div className="container text-center">
+            <h3>Login to continue</h3>
+
+            <form onSubmit={handleFormSubmit}>
+                <div>
+                    <label>Email:</label>
+                    <input 
+                        className="form-control" 
+                        type='text' 
+                        name="email"
+                        onChange={handleChange}
+                    />
+                    {errors.email && (errors.email)}
+                </div>
+
+                <div>
+                    <label>Password:</label>
+                    <input 
+                        className="form-control" 
+                        type='password' 
+                        name="password" 
+                        onChange={handleChange}
+                    />
+                    {errors.password && (errors.password)}
+                </div>
+
+                <div>
+                    <button className="btn btn-primary">Login</button>
+                </div>
+            </form>
         </div>
     );
 }
